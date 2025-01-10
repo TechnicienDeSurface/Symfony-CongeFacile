aa8a686ba770e6b0b1e95eb8cf3429cb9af04d63
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-01-10 17:14:21 +0100

    Création des entités restantes

---
+++ src/Entity/Person.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonRepository::class)]
+class Person
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $last_name = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $first_name = null;
+
+    #[ORM\Column]
+    private ?int $manager_id = null;
+
+    #[ORM\Column]
+    private ?int $department_id = null;
+
+    #[ORM\Column]
+    private ?int $position_id = null;
+
+    #[ORM\Column]
+    private ?bool $alert_new_request = null;
+
+    #[ORM\Column]
+    private ?bool $alert_on_answer = null;
+
+    #[ORM\Column]
+    private ?bool $alert_before_vacation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): static
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): static
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getManagerId(): ?int
+    {
+        return $this->manager_id;
+    }
+
+    public function setManagerId(int $manager_id): static
+    {
+        $this->manager_id = $manager_id;
+
+        return $this;
+    }
+
+    public function getDepartmentId(): ?int
+    {
+        return $this->department_id;
+    }
+
+    public function setDepartmentId(int $department_id): static
+    {
+        $this->department_id = $department_id;
+
+        return $this;
+    }
+
+    public function getPositionId(): ?int
+    {
+        return $this->position_id;
+    }
+
+    public function setPositionId(int $position_id): static
+    {
+        $this->position_id = $position_id;
+
+        return $this;
+    }
+
+    public function isAlertNewRequest(): ?bool
+    {
+        return $this->alert_new_request;
+    }
+
+    public function setAlertNewRequest(bool $alert_new_request): static
+    {
+        $this->alert_new_request = $alert_new_request;
+
+        return $this;
+    }
+
+    public function isAlertOnAnswer(): ?bool
+    {
+        return $this->alert_on_answer;
+    }
+
+    public function setAlertOnAnswer(bool $alert_on_answer): static
+    {
+        $this->alert_on_answer = $alert_on_answer;
+
+        return $this;
+    }
+
+    public function isAlertBeforeVacation(): ?bool
+    {
+        return $this->alert_before_vacation;
+    }
+
+    public function setAlertBeforeVacation(bool $alert_before_vacation): static
+    {
+        $this->alert_before_vacation = $alert_before_vacation;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?bool $enabled = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $role = null;
+
+    #[ORM\Column]
+    private ?int $person_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function isEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): static
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getPersonId(): ?int
+    {
+        return $this->person_id;
+    }
+
+    public function setPersonId(int $person_id): static
+    {
+        $this->person_id = $person_id;
+
+        return $this;
+    }
+}
