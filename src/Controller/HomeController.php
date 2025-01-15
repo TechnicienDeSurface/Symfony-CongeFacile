7746be31d34418426f5568326dab7387300002f0
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-01-15 19:23:05 +0100

    Création vue accueil + méthode accueil dans homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,27 @@ class HomeController extends AbstractController
             'page' => 'home',
         ]);
     }
+
+    #[Route('/accueil', name: 'accueil')]
+    public function accueil(): Response
+    {
+        // $paragraphe = "CongéFacile est votre nouvel outil dédié à la gestion des congés au sein de l'entreprise.<br>
+        // Plus besoin d'échanges interminables ou de formulaires papier : en quelques clics, vous pouvez gérer<br>
+        // vos absences en toute transparence et simplicité." ; 
+        $paragraphe = "CongéFacile est votre nouvel outil dédié à la gestion des congés au sein de l'entreprise.
+        Plus besoin d'échanges interminables ou de formulaires papier : en quelques clics, vous pouvez gérer
+        vos absences en toute transparence et simplicité." ; 
+        $texte1 = "j'effectue ma demande de congés"; 
+        $texte2 = "Mon manager valide ou refuse la demande" ; 
+        $texte3 = "Je consulte l'historique de mes demandes"; 
+        return $this->render('accueil/accueil.html.twig', [
+            'page' => 'accueil',
+            'titre'=>'CongéFacile', 
+            'paragraphe' => $paragraphe, 
+            'titre2' => 'Etapes', 
+            'texte1' =>$texte1, 
+            'texte2 '=>$texte2,
+            'texte3' =>$texte3,  
+        ]); 
+    }
 }
