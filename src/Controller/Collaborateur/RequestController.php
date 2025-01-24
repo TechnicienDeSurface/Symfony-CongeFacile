494ff4d6ec400deab3b2f80e60b7b8d05b651b72
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-01-24 19:08:49 +0100

    Développement méthode new-request dans RequestController + Création du formulaire de Request

--- src/Controller/Collaborateur/RequestController.php
+++ src/Controller/Collaborateur/RequestController.php
@@ -2,16 +2,44 @@
 
 namespace App\Controller\Collaborateur;
 
+use App\Repository\RequestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request as RequestFondation;
+use App\Entity\Request ; 
+use App\Form\RequestType ; 
 
 class RequestController extends AbstractController
 {
     //PAGE NOUVELLE DEMANDE "COLLABORATEUR"
     #[Route('/new-request', name: 'app_new_request')]
-    public function viewNewRequest(): Response
+    public function viewNewRequest(RequestFondation $request_bd, RequestRepository $repository, ManagerRegistry $registry): Response
     {
+        $request = new Request() ; 
+        $form = $this->createForm(RequestType::class,$request); 
+        $form->handleRequest($request_bd); 
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                try{
+                    $request_bd->cookies->all();
+                    // Si valide : j'enregistre les données dans la BDD.
+                    $registry->getManager()->persist($request);
+                    $registry->getManager()->flush();
+                    $this->addFlash('success', 'Success to add product') ; 
+                }catch(NotFoundHttpException $e ){
+                    $this->addFlash('error', 'Error to add product') ; 
+                }
+                // Faire une redirection vers le formulaire de modification.
+            } else {
+                // Sinon j'affiche un message d'erreur
+                $this->addFlash('error', 'Error to add product') ; 
+            }
+        }else{
+        }
+
         return $this->render('collaborateur/new_request.html.twig', [
             'page' => 'new-request',
         ]);
---
+++ src/Form/RequestType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Department;
+use App\Entity\Person;
+use App\Entity\Request;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RequestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('created_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('start_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('end_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('receipt_file')
+            ->add('comment')
+            ->add('answer_comment')
+            ->add('answer')
+            ->add('answer_at', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('request_type', EntityType::class, [
+                'class' => self::class,
+                'choice_label' => 'id',
+            ])
+            ->add('collaborator', EntityType::class, [
+                'class' => Person::class,
+                'choice_label' => 'id',
+            ])
+            ->add('department', EntityType::class, [
+                'class' => Department::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Request::class,
+        ]);
+    }
+}
