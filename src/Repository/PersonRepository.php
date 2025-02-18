6ed33ca31c2d50bc3b8b34bd489f47b1cc7575af
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-02-18 20:59:15 +0100

    Création des méthodes de contrôleurs de recherche équipe + Détails + Filtres

--- src/Repository/PersonRepository.php
+++ src/Repository/PersonRepository.php
@@ -16,28 +16,78 @@ class PersonRepository extends ServiceEntityRepository
         parent::__construct($registry, Person::class);
     }
 
-    //    /**
-    //     * @return Person[] Returns an array of Person objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+       /**
+        * @return Person[] Returns an array of Person objects
+        */
+       public function findByFirstname($value): array
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.firstname = :val')
+               ->setParameter('val', $value)
+               ->orderBy('p.id', 'ASC')
+               ->setMaxResults(10)
+               ->getQuery()
+               ->getResult()
+           ;
+       }
 
-    //    public function findOneBySomeField($value): ?Person
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+       public function findByLasname($value): array
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.lastname = :val')
+               ->setParameter('val', $value)
+               ->orderBy('p.id', 'ASC')
+               ->setMaxResults(10)
+               ->getQuery()
+               ->getResult()
+           ;
+       }
+
+       public function findByEmail($value): array
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.email = :val')
+               ->setParameter('val', $value)
+               ->orderBy('p.id', 'ASC')
+               ->setMaxResults(10)
+               ->getQuery()
+               ->getResult()
+           ;
+       }
+
+       public function findByFirstPoste($value): array
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.poste = :val')
+               ->setParameter('val', $value)
+               ->orderBy('p.id', 'ASC')
+               ->setMaxResults(10)
+               ->getQuery()
+               ->getResult()
+           ;
+       }
+
+       public function findByNbConge($value): array
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.firstname = :val')
+               ->setParameter('val', $value)
+               ->orderBy('p.id', 'ASC')
+               ->setMaxResults(10)
+               ->getQuery()
+               ->getResult()
+           ;
+       }
+
+       public function findOneBySomeField($value): ?Person
+       {
+           return $this->createQueryBuilder('p')
+               ->andWhere('p.exampleField = :val')
+               ->setParameter('val', $value)
+               ->getQuery()
+               ->getOneOrNullResult()
+           ;
+       }
+
+    
 }
