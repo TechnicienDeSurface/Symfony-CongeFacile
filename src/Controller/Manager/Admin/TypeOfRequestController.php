b8b77bb5acdda9ff397fbd419fa946a6c2bf3a59
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-02-21 08:32:09 +0100

    Données test

--- src/Controller/Manager/Admin/TypeOfRequestController.php
+++ src/Controller/Manager/Admin/TypeOfRequestController.php
@@ -12,8 +12,22 @@ class TypeOfRequestController extends AbstractController
     #[Route('/administration-type-de-demande', name: 'app_administration_type_of_request')]
     public function viewTypeOfRequest(): Response
     {
+         $demandes = [
+            ['nom' => 'Congé annuel', 'nb' => 15],
+            ['nom' => 'Congé maladie', 'nb' => 8],
+            ['nom' => 'Congé maternité', 'nb' => 5],
+            ['nom' => 'Congé paternité', 'nb' => 3],
+            ['nom' => 'Congé sans solde', 'nb' => 2],
+            ['nom' => 'Congé sabbatique', 'nb' => 1],
+            ['nom' => 'Congé parental', 'nb' => 4],
+            ['nom' => 'Congé de formation', 'nb' => 6],
+            ['nom' => 'Congé de déménagement', 'nb' => 2],
+            ['nom' => 'Congé de mariage', 'nb' => 3],
+            // Ajoutez d'autres demandes ici
+        ];
         return $this->render('manager/admin/type-of-request/type_of_request.html.twig', [
             'page' => 'administration-type-of-request',
+            'demandes' => $demandes, 
         ]);
     }
 
