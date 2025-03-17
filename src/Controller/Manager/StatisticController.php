7bd6151b45dc4ab4757fee0e182a7d2aeda58214
Adrien - Leclère <adrien.leclere@orange.fr>
2025-03-17 16:14:41 +0100

    UPDATE - Ajout du graphique en camembert pour chartjs

--- src/Controller/Manager/StatisticController.php
+++ src/Controller/Manager/StatisticController.php
@@ -2,24 +2,36 @@
 
 namespace App\Controller\Manager;
 
+use App\Entity\Request as RequestEntity;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StatisticController extends AbstractController
 {
-    //PAGE DES STATISTIQUES DE CONGES ACCEPTER OU REFUSER
     #[Route('/statistic-manager', name: 'app_statistic')]
-    public function viewStatistic(): Response
+    public function viewStatistic(EntityManagerInterface $entityManagerInterface): Response
     {
+        $requests = $entityManagerInterface->getRepository(RequestEntity::class)->findAll();
+        $requestCounts = [];
+
+        // Compter les types de demandes
+        foreach ($requests as $requestEntity) {
+            $type = $requestEntity->getRequestType()?->getName();
+            if ($type) {
+                $requestCounts[$type] = ($requestCounts[$type] ?? 0) + 1;
+            }
+        }
+
         $data = [
-            'labels' => ['Catégorie A', 'Catégorie B', 'Catégorie C'],
-            'values' => [30, 50, 20]
+            'labels' => array_keys($requestCounts),
+            'values' => array_values($requestCounts),
         ];
 
         return $this->render('manager/statistic.html.twig', [
             'page' => 'statistic-manager',
-            'chartsCamembert' => json_encode($data),
+            'requestData' => json_encode($data),
         ]);
     }
 }
