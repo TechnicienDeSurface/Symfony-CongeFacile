98a0c8711aa72a6149d222eb0fbac64e4d1bd48e
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-03-18 17:26:11 +0100

    Design toggle part1

--- src/Form/PreferenceType.php
+++ src/Form/PreferenceType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PreferenceType extends AbstractType
 {
@@ -24,6 +25,11 @@ class PreferenceType extends AbstractType
             ->add('alert_before_vacation', CheckboxType::class, [
                 'label'    => 'Recevoir un rappel par email lorsqu’un congé arrive la semaine prochaine',
                 'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer mes préférences', 
+                'attr' => ['class'=>'w-full text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 transition"', 
+                            'style' => 'background-color : #004C6C ; '], 
             ]);
     }
 
