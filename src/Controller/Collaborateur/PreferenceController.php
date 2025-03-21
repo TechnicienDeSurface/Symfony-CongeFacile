1f6db8b6e32ea29e053a3a070b9ec19584fc061d
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-03-21 18:12:25 +0100

    Méthode preferenceCollaborateur

--- src/Controller/Collaborateur/PreferenceController.php
+++ src/Controller/Collaborateur/PreferenceController.php
@@ -5,17 +5,47 @@ namespace App\Controller\Collaborateur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\PersonRepository ; 
 use App\Form\PreferenceType ; 
-use App\Entity\Person ; 
+use App\Entity\Person ;
+use App\Entity\User ;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class PreferenceController extends AbstractController
 {
     //PAGE PREFERENCE "COLLABORATEUR"
     #[Route('/preference-collaborateur', name: 'app_preference_collaborateur')]
-    public function viewPreferenceCollaborateur(): Response
+    public function viewPreferenceCollaborateur(ManagerRegistry $registry, PersonRepository $repository, Request $request ): Response
     {
-        $person = new Person ; 
+        $id = null ; 
+        /** @var User $user */ 
+        $user = $this->getUser() ; //récupère l'objet de l'utilisateur connecté  
+
+        $userHost = $user->getPerson() ; 
+        $id = $userHost->getId();
+        
+        $person = $repository->find($id) ; 
+        if(!$person){
+            throw $this->createNotFoundException('No person found for id ' . $id);
+        }
         $form = $this->createForm(PreferenceType::class, $person ) ; 
+        $form->handleRequest($request) ; 
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                //dd($request->cookies->all());
+                // Si valide : j'enregistre les données dans la BDD.
+                $request->cookies->all();
+                $registry->getManager()->flush();
+
+                // Faire une redirection vers le formulaire de modification.
+            } else {
+                // Sinon j'affiche un message d'erreur
+                dd('Formulaire invalide');
+            }
+        }
 
         return $this->render('collaborateur/preference.html.twig', [
             'page' => 'preference-collaborateur',
