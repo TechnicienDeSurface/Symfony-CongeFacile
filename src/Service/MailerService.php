a63c1470e0dd1d98613044f4eb47df2c87849dc1
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-03-23 12:36:02 +0100

    Configuration mailer et event

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -3,10 +3,16 @@
 namespace App\Service;
 
 use Symfony\Component\Mailer\MailerInterface;
+use App\Entity\User ; 
+use App\Repository\UserRepository ; 
+use App\Repository\RequestRepository ; 
 use Symfony\Component\Mime\Email;
+use App\Event\LeaveReminderEvent;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class MailerService {
     private $mailer;
+    private $eventDispatcher;
 
     public function __construct(MailerInterface $mailer) {
         $this->mailer = $mailer;
@@ -21,5 +27,17 @@ class MailerService {
             ->html($html);
 
         $this->mailer->send($email);
+    }
+
+    public function checkLeaves(UserRepository $repository, RequestRepository $request_repository) {
+        $oneWeekLater = (new \DateTime())->modify('+1 week');
+        $users = $repository->All() ; 
+        foreach ($users as $row) {
+            $person = $row->getPerson();
+            $request = $request_repository->find($person->getId()) ; 
+            if ($request->getStartDate() == $oneWeekLater && $person->getAlertOnBeforeVacation()) {
+                $this->eventDispatcher->dispatch(new LeaveReminderEvent($row), LeaveReminderEvent::NAME);
+            }
+        }
     }
 }
