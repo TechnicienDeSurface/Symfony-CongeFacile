4884ec932f2700bce8cf15d5299fe95f4ed59dd1
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-03-31 12:26:35 +0200

    Configuration mailcommand

--- src/Command/MailCommand.php
+++ src/Command/MailCommand.php
@@ -7,7 +7,7 @@ use App\Entity\User ;
 use App\Entity\Request ;
 use App\Service\MailerService;
 use Symfony\Component\Mailer\Transport;
-use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface as Mailer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -51,53 +51,53 @@ class MailCommand extends Command
             $io->note(sprintf('You passed an argument: %s', $arg1));
 
             // Configurer le transport de mail
-$transport = Transport::fromDsn($_ENV['MAILER_DSN']);
-$mailer = new Mailer($transport);
-$mailerService = new MailerService($mailer);
+            $transport = Transport::fromDsn($_ENV['MAILER_DSN']);
+            $mailer = new Mailer($transport);
+            $mailerService = new MailerService($mailer);
 
-// Récupérer les repositories des entités
-$userRepository = $this->entityManager->getRepository(User::class);
-$personRepository = $this->entityManager->getRepository(Person::class);
-$requestRepository = $this->entityManager->getRepository(Request::class);
+            // Récupérer les repositories des entités
+            $userRepository = $this->entityManager->getRepository(User::class);
+            $personRepository = $this->entityManager->getRepository(Person::class);
+            $requestRepository = $this->entityManager->getRepository(Request::class);
 
-// Récupérer toutes les données des entités
-$users = $userRepository->findBy([],[]);
-$persons = $personRepository->findBy([],[]);
-$requests = $requestRepository->findBy([],[]);
+            // Récupérer toutes les données des entités
+            $users = $userRepository->findBy([],[]);
+            $persons = $personRepository->findBy([],[]);
+            $requests = $requestRepository->findBy([],[]);
 
-// Planifier l'envoi d'email toutes les minutes
+            // Planifier l'envoi d'email toutes les minutes
 
-    foreach ($persons as $row) {
-        foreach($requests as $request){
-            if($request->collaborator->id == $row->id){
-                $ask = $request; 
+            foreach ($persons as $row) {
+                foreach($requests as $request){
+                    if($request->collaborator->id == $row->id){
+                        $ask = $request; 
+                    }
+                }
+                foreach($users as $user){
+                    if($user->person->id == $row->id){
+                        $userPerson = $user; 
+                        $to = $userPerson->email; 
+                    }
+                }
+                
+                // Logique pour chaque personne
+                if(isset($to)){
+                    if($row->alert_before_vacations == true){
+                        $startAt = $request->getStartAt(); 
+                        $now = new \DateTime();
+                        $interval = $now->diff($startAt); //Calculer la différence entre 2 dates
+                        if ($interval->days == 7 && $interval->invert == 0) {
+                            $mailerService->sendEmail($to, 'Rappel de congé', 'Vous prenez vos congé dans 1 semaine. '); 
+                        }   
+                    }
+                    if($row->alert_on_answer == true){
+                        if($ask->answer === true){
+                            $mailerService->sendEmail($to, 'Demande de congé - Réponse', 'Votre manager a répondu à votre demande de congé.');
+                        } 
+                    }
+                }
+
             }
-        }
-        foreach($users as $user){
-            if($user->person->id == $row->id){
-                $userPerson = $user; 
-                $to = $userPerson->email; 
-            }
-        }
-        
-        // Logique pour chaque personne
-        if(isset($to)){
-            if($row->alert_before_vacations == true){
-                $startAt = $request->getStartAt(); 
-                $now = new \DateTime();
-                $interval = $now->diff($startAt); //Calculer la différence entre 2 dates
-                if ($interval->days == 7 && $interval->invert == 0) {
-                    $mailerService->sendEmail($to, 'Rappel de congé', 'Vous prenez vos congé dans 1 semaine. '); 
-                }   
-            }
-            if($row->alert_on_answer == true){
-                if($ask->answer === true){
-                    $mailerService->sendEmail($to, 'Demande de congé - Réponse', 'Votre manager a répondu à votre demande de congé.');
-                } 
-            }
-        }
-
-    }
             $output->writeln('Email envoyé avec succès!');
 
         return Command::SUCCESS;
