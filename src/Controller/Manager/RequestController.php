286ab6ea4fe6fa35e07770c92e8fece5ac1fac1b
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-04-03 15:33:44 +0200

    Corrections front Historique demandes manager

--- src/Controller/Manager/RequestController.php
+++ src/Controller/Manager/RequestController.php
@@ -5,6 +5,10 @@ namespace App\Controller\Manager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\RequestRepository ; 
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\HttpFoundation\Request;
 
 class RequestController extends AbstractController
 {
@@ -28,10 +32,12 @@ class RequestController extends AbstractController
 
     //PAGE HISTORIQUE DES DEMANDES
     #[Route('/history-request', name: 'app_history_request')]
-    public function viewRequestHistory(): Response
+    public function viewRequestHistory(Request $request, RequestRepository $repository, int $page = 1 ): Response
     {
+        $requests = $repository->findBy([],[]) ;
         return $this->render('manager/history_request.html.twig', [
             'page' => 'history-request',
+            'requests' => $requests,
         ]);
     }
 }
