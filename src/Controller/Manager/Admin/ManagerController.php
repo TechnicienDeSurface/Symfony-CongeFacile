218c5c9527d30c2b7aa3d4ff1eb2ff01364b67a6
Noah <noah60ise@gmail.com>
2025-05-07 22:06:53 +0200

    correction erreur 404 detail manager dans l'administration

--- src/Controller/Manager/Admin/ManagerController.php
+++ src/Controller/Manager/Admin/ManagerController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller\Manager\Admin;
 
-use App\Entity\User; 
-use App\Entity\Person; 
+use App\Entity\User;
+use App\Entity\Person;
 use App\Form\ManagerType;
-use App\Repository\UserRepository; 
+use App\Repository\UserRepository;
 use App\Form\FilterManagerFormType;
 use App\Repository\PersonRepository;
-use App\Repository\PositionRepository; 
+use App\Repository\PositionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\DepartmentRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\SecurityBundle\Security; 
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -24,9 +24,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ManagerController extends AbstractController
 {
-    public function __construct(private UserPasswordHasherInterface $passwordHasher){
-        
-    }
+    public function __construct(private UserPasswordHasherInterface $passwordHasher) {}
 
     //PAGE MANAGER VIA ADMINISTRATION MANAGER
     #[IsGranted('ROLE_MANAGER')]
@@ -61,7 +59,7 @@ class ManagerController extends AbstractController
         $pagerfanta->setMaxPerPage(10);
 
         try {
-            if($form->isSubmitted()) {
+            if ($form->isSubmitted()) {
                 $page = 1;
             }
             $pagerfanta->setCurrentPage($page);
@@ -82,32 +80,31 @@ class ManagerController extends AbstractController
     //PAGE AJOUTER MANAGER VIA ADMINISTRATION MANAGER
     #[IsGranted('ROLE_MANAGER')]
     #[Route('/administration-ajouter-manager', name: 'app_administration_ajouter_manager')]
-    public function addManager(UserPasswordHasherInterface $hash, ManagerRegistry $registry, Security $security,Request $request, PositionRepository $position_repository, PersonRepository $person_repository, DepartmentRepository $department_repository, UserRepository $user_repository): Response
+    public function addManager(UserPasswordHasherInterface $hash, ManagerRegistry $registry, Security $security, Request $request, PositionRepository $position_repository, PersonRepository $person_repository, DepartmentRepository $department_repository, UserRepository $user_repository): Response
     {
-        $users = $user_repository->findBy([],[]);
-        $exist_email = false; 
-        $manager = New User() ; 
-        $manager = $security->getUser() ;
+        $users = $user_repository->findBy([], []);
+        $exist_email = false;
+        $manager = new User();
+        $manager = $security->getUser();
         if (!$manager instanceof \App\Entity\User) {
             throw new \LogicException('L\'utilisateur connecté n\'est pas une instance de App\Entity\User.');
         }
-        $form = $this->createForm(ManagerType::class, null,['submit_label' => 'Ajouter']); 
-        $form->handleRequest($request); 
-        if($form->isSubmitted())
-        {
-            if($form->isValid()){
+        $form = $this->createForm(ManagerType::class, null, ['submit_label' => 'Ajouter']);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
                 $formData = $form->getData();
-                if(!empty($formData['email'])){
-                    foreach($users as $row){
-                        if($row->getEmail() == $formData['email']){
-                            $exist_email = true; 
+                if (!empty($formData['email'])) {
+                    foreach ($users as $row) {
+                        if ($row->getEmail() == $formData['email']) {
+                            $exist_email = true;
                         }
                     }
                 }
-                if($formData['newPassword'] == $formData['confirmPassword']){
-                    if($exist_email === false){
-                        try{
-                            $new_manager= new Person();
+                if ($formData['newPassword'] == $formData['confirmPassword']) {
+                    if ($exist_email === false) {
+                        try {
+                            $new_manager = new Person();
                             $new_manager->setFirstName($formData['first_name']);
                             $new_manager->setLastName($formData['last_name']);
                             $new_manager->setDepartment($formData['department']);
@@ -119,13 +116,14 @@ class ManagerController extends AbstractController
                             $registry->getManager()->persist($new_manager);
                             $registry->getManager()->flush();
                             $this->addFlash('success', 'Succès pour ajouter le manager');
-                        }catch(\Exception $e){
-                            $this->addFlash('error', 'Erreur pour l\'ajout du manager'); 
-                        }try{
+                        } catch (\Exception $e) {
+                            $this->addFlash('error', 'Erreur pour l\'ajout du manager');
+                        }
+                        try {
                             $person = $registry->getManager()->getRepository(Person::class)->find($new_manager->getId());
-                            $new_user = New User();
+                            $new_user = new User();
                             $new_user->setEmail($formData['email']);
-                            $password_hash = $this->passwordHasher->hashPassword($new_user, $formData['newPassword']) ;
+                            $password_hash = $this->passwordHasher->hashPassword($new_user, $formData['newPassword']);
                             $new_user->setPassword($password_hash);
                             $new_user->setPerson($person);
                             $new_user->setRoles([1 => "ROLE_MANAGER"]);
@@ -135,141 +133,112 @@ class ManagerController extends AbstractController
                             $registry->getManager()->flush();
                             $this->addFlash('success', 'Succès pour ajouter de l\'utilisateur');
                             return $this->redirectToRoute('app_administration_manager');
-                        }catch(\Exception $e ){
-                            $this->addFlash('error', 'Erreur pour l\'ajout de l\'utilisateur'); 
+                        } catch (\Exception $e) {
+                            $this->addFlash('error', 'Erreur pour l\'ajout de l\'utilisateur');
                         }
-                    }else{
-                        $this->addFlash('error','Erreur l\'email existe déjà');    
+                    } else {
+                        $this->addFlash('error', 'Erreur l\'email existe déjà');
                     }
-                }else{
-                    $this->addFlash('error','Erreur la confirmation mot de passe n\'est pas identique au nouveau mot de passe');
+                } else {
+                    $this->addFlash('error', 'Erreur la confirmation mot de passe n\'est pas identique au nouveau mot de passe');
                 }
             }
         }
-        
+
         return $this->render('manager/admin/manager/add_manager.html.twig', [
             'page' => 'administration-ajouter-manager',
-            'manager'=>$manager,
-            'form'=>$form,
+            'manager' => $manager,
+            'form' => $form,
             'submit_label' => 'Ajouter',
         ]);
     }
 
-    //PAGE DETAIL MANAGER VIA ADMINISTRATION MANAGER
     #[IsGranted('ROLE_MANAGER')]
-    #[Route('/administration-detail-manager/{id}', name: 'app_administration_detail_manager', methods: ['GET','POST'])]
-    public function editManager(DepartmentRepository $department_repository, PersonRepository $repository, int $id, EntityManagerInterface $entityManager,Request $request,UserRepository $user_repository): Response
-    {
-        $users = $user_repository->findBy([],[]);
-        $exist_email = false; 
-        $manager = $repository->find($id);
-        if(!$manager){
-            throw $this->createNotFoundException('No manager found for id ' . $id);
+    #[Route('/administration-detail-manager/{id}', name: 'app_administration_detail_manager', methods: ['GET', 'POST'])]
+    public function editManager(
+        DepartmentRepository $departmentRepository,
+        PersonRepository $personRepository,
+        int $id,
+        EntityManagerInterface $entityManager,
+        Request $request,
+        UserRepository $userRepository
+    ): Response {
+        $user = $userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Aucun utilisateur trouvé pour l\'ID ' . $id);
         }
-        $form = $this->createForm(ManagerType::class, null, [
-            'csrf_token_id' => 'submit', //Ajout du token csrf id car formulaire non lié à une entité 
-            'submit_label' => 'Mettre à jour'
+
+        $manager = $user->getPerson();
+
+        if (!$manager) {
+            throw $this->createNotFoundException('Aucune personne liée à cet utilisateur.');
+        }
+
+        $existEmail = false;
+
+        $form = $this->createForm(ManagerType::class, [
+            'email' => $user->getEmail(),
+            'first_name' => $manager->getFirstName(),
+            'last_name' => $manager->getLastName(),
+            'department' => $manager->getDepartment(),
         ]);
+
         $form->handleRequest($request);
-        $users = $user_repository->findBy([],[]);
-        foreach($users as $row){
-            if($row->getPerson()->getId() === $id){
-                $user = $row;
-            }
-        }
-        if($form->isSubmitted()){
-            if($form->isValid()){
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
                 $formData = $form->getData();
-                if(!empty($formData['email']) && $formData['email'] != $manager->getUser()->getEmail()){
-                    foreach($users as $row){
-                        if($row->getEmail() == $formData['email']){
-                            $exist_email = true; 
-                        }
+
+                // Vérification de l'email existant
+                if (!empty($formData['email']) && $formData['email'] !== $user->getEmail()) {
+                    $existingUser = $userRepository->findOneBy(['email' => $formData['email']]);
+                    if ($existingUser) {
+                        $existEmail = true;
                     }
                 }
-                if($exist_email == false){
-                    if(empty($formData['newPassword']) && empty($formData['confirmPassword'])){
-                        try{
-                            if($manager->getFirstName() != $formData['first_name']){
-                                $manager->setFirstName($formData['first_name']);
-                            }
-                            if($manager->getLastName() != $formData['last_name']){
-                                $manager->setLastName($formData['last_name']);
-                            }
-                            if($manager->getDepartment != $formData['department']){
-                                $department = $department_repository->find($formData['department']);
-                                $manager->setDepartment($department);
-                            }
-                            $entityManager->persist($manager);
-                            $entityManager->flush();
-                            $this->addFlash('success', 'Succès pour la mise à jour du manager');
-                        }catch(\Exception $e){
 
-                        }try{
-                            if($user->getEmail() != $formData['email']){
-                                $user->setEmail($formData['email']);
-                            } 
-                            $entityManager->persist($user);
-                            $entityManager->flush();
-                            $this->addFlash('success', 'Succès pour la mise à jour de l\'utilisateur');
-                            return $this->redirectToRoute('app_administration_manager');
-                        }catch(\Exception $e ){
-                            $this->addFlash('error', 'Erreur pour la mise à jour de l\'utilisateur'); 
-                        }
-                    }else{
-                        if($formData['newPassword'] == $formData['confirmPassword']){
-                            try{
-                                if($manager->getFirstName() != $formData['first_name']){
-                                    $manager->setFirstName($formData['first_name']);
-                                }
-                                if($manager->getLastName() != $formData['last_name']){
-                                    $manager->setLastName($formData['last_name']);
-                                }
-                                if($manager->getDepartment != $formData['department']){
-                                    $department = $department_repository->find($formData['department']);
-                                    $manager->setDepartment($department);
-                                }
-                                $entityManager->persist($manager);
-                                $entityManager->flush();
-                                $this->addFlash('success', 'Succès pour la mise à jour le manager');
-                            }catch(\Exception $e){
+                if (!$existEmail) {
+                    try {
+                        // Mise à jour de la personne
+                        $manager->setFirstName($formData['first_name']);
+                        $manager->setLastName($formData['last_name']);
+                        $manager->setDepartment(
+                            $departmentRepository->find($formData['department'])
+                        );
+                        $entityManager->persist($manager);
 
-                            }try{
-                                $password_hash = $this->passwordHasher->hashPassword($user, $formData['newPassword']) ;
-                                if($user->getEmail() != $formData['email']){
-                                    $user->setEmail($formData['email']);
-                                }
-                                if($password_hash != $user->getPassword()){
-                                    $user->setPassword($password_hash);
-                                }  
-                                $entityManager->persist($user);
-                                $entityManager->flush();
-                                $this->addFlash('success', 'Succès pour la mise à jour de l\'utilisateur');
-                                return $this->redirectToRoute('app_administration_manager');
-                            }catch(\Exception $e ){
-                                $this->addFlash('error', 'Erreur pour la mise à jour de l\'utilisateur'); 
+                        // Mise à jour de l'utilisateur
+                        $user->setEmail($formData['email']);
+
+                        if (!empty($formData['newPassword']) && !empty($formData['confirmPassword'])) {
+                            if ($formData['newPassword'] === $formData['confirmPassword']) {
+                                $passwordHash = $this->passwordHasher->hashPassword($user, $formData['newPassword']);
+                                $user->setPassword($passwordHash);
+                            } else {
+                                $this->addFlash('error', 'La confirmation du mot de passe ne correspond pas.');
                             }
-                        }else{
-                            $this->addFlash('error','Erreur la confirmation mot de passe n\'est pas identique au nouveau mot de passe');
                         }
+
+                        $entityManager->persist($user);
+                        $entityManager->flush();
+
+                        $this->addFlash('success', 'Manager mis à jour avec succès.');
+                        return $this->redirectToRoute('app_administration_manager');
+                    } catch (\Exception $e) {
+                        $this->addFlash('error', 'Erreur lors de la mise à jour.');
                     }
-                }else{
-                    $this->addFlash('error','Erreur l\'email existe déjà');  
-                }   
+                } else {
+                    $this->addFlash('error', 'L\'email existe déjà.');
+                }
             }
-        }else{
-            $form = $this->createForm(ManagerType::class, [
-                'email' => $user->getEmail(),
-                'first_name' => $manager->getFirstName(),
-                'last_name' => $manager->getLastName(),
-                'department' => $manager->getDepartment(),
-            ]);
         }
+
         return $this->render('manager/admin/manager/detail_manager.html.twig', [
             'page' => 'administration-manager',
-            'form'=>$form, 
-            'manager'=>$manager,
-            'user'=>$user,
+            'form' => $form,
+            'manager' => $manager,
+            'user' => $user,
         ]);
     }
 }
